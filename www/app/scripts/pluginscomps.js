54bd97c819fdb2c8aa5b6904c3724ddf684e3cd3
MurageKabui <dennisk@zainahtech.com>
2026-07-09 15:42:03 +0300

    apk in artifacts and minification before commit

--- www/app/scripts/pluginscomps.js
+++
@@ -1,6 +0,0 @@
-/*!
- This file is part of DecSoft App Builder.
- Visit our website for license information.
- Copyright © DecSoft Utils - All rights reserved.
- Visit our website at: https://www.decsoftutils.com
- */
--- www/cordova.js
+++
@@ -1,6 +0,0 @@
-/*!
-This file is part of DecSoft App Builder.
-Visit our website for license information.
-Copyright © DecSoft Utils - All rights reserved.
-Visit our website at: https://www.decsoftutils.com
-*/
